03afac39da630399d3c3d94a123144aaf46f6ec0
Andrew Carver <drew.carver@outlook.com>
2024-05-05 15:21:40 -0400

    get the dialog closing after save

--- Program.fs
+++ Program.fs
@@ -25,10 +25,41 @@ let domain = "dev-fu83rki86r8dd5bd.us.auth0.com"
 
 let ticketModal ticketId = dialog [] [ h1 [] [ str "Test" ] ]
 
+let addTicketDialog =
+    htmlView (
+        dialog
+            [ _id "add-ticket-dialog"
+              _class "modal"
+              attr "hx-on:htmx:load" "document.getElementById('add-ticket-dialog').showModal()" ]
+            [ header
+                  [ _class "modal__header" ]
+                  [ h1 [] [ str "Add Ticket" ]
+                    button
+                        [ _class "btn"
+                          attr "aria-label" "Close"
+                          _onclick "document.getElementById('add-ticket-dialog').close()" ]
+                        [ i [ _class "fa fa-times fa-2x" ] [] ] ]
+              form
+                  [ attr "hx-post" "/tickets"
+                    _class "ticket-form"
+                    _formmethod "dialog"
+                    attr "hx-indicator" "#save-ticket-spinner" ]
+                  [ input [ _required; _name "title"; _placeholder "Title" ]
+                    input [ _required; _name "description"; _placeholder "Description" ]
+                    input [ _required; _name "status"; _placeholder "Status" ]
+                    button [ _type "submit" ] [ str "Save" ] ]
+              div
+                  [ _id "save-ticket-spinner"; _class "htmx-indicator" ]
+                  [ div [ _class "lds-ring" ] [ div [] []; div [] []; div [] []; div [] [] ]
+                    p [] [ str "Saving changes..." ] ] ]
+
+    )
+
 let endpoints =
     [ GET [ routef "/tickets/%s" (fun ticketId -> htmlView (ticketModal ticketId)) ]
       POST [ route "/tickets" Todo.addTicket ]
-      GET [ route "/tickets" Todo.listTickets ] ]
+      GET [ route "/tickets" Todo.listTickets ]
+      GET [ route "/show-add-dialog" addTicketDialog ] ]
 
 let configureApp (appBuilder: IApplicationBuilder) =
     appBuilder
